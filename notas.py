b13e2fa7cfcbe8846e969c109ca8f946e87caa4d
EmersonRian <emersonrianlima06@gmail.com>
2026-03-03 15:47:00 -0800

    notas

--- notas.py
+++ notas.py
@@ -1,12 +1,46 @@
 
 #  11 - Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas,
 #  imprima na tela o nome do aluno e 
-# nota_portugues = input("Sua nota de Portugues: ")
-# nota_historia = input("Sua nota de Historia")
+
+#nota do {semetre}° semestre :
+
+semestre = 1
+notas = []
+
+
+print("Qual nome do aluno?")
+aluno = input()
+
+while semestre < 5:
+    notas.append(float(input(f"nota do {semestre}° semestre: ")))
+    semestre += 1
+
+def media_das_notas():
+    media_das_notas = sum(notas) / len(notas)
+
+    if media_das_notas >= 7:
+        print(f"Aluno {aluno} passou de ano")
+
+        for a, b in enumerate(notas, start=1):   
+            print(f"Nota {b} no {a}° semestre")
+
+        print(f"Com media de {media_das_notas}")
+
+    if media_das_notas < 7:
+        print(f"Aluno {aluno} esta de recuperação")
+
+        for a, b in enumerate(notas, start=1):   
+            print(f"Nota {b} no {a}° semestre")
+            
+        print(f"Com media de {media_das_notas}")
+
+media_das_notas()
 
 
 
-depresivo = range(11)
 
-for i in reversed(depresivo):
-    print(i)
+
+
+
+
+
