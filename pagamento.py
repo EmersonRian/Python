9948ba8ad95ba5cc280e12988b4c4b7c6116427c
EmersonRian <emersonrianlima06@gmail.com>
2026-03-05 13:40:41 -0800

    pagamento

--- pagamento.py
+++ pagamento.py
@@ -15,3 +15,57 @@
 #  3 - Parcelado no cartão em duas vezes, preço normal do produto sem juros
 
 #  4 - Parcelado no cartão em três vezes ou mais, preço normal do produto mais juros de 10%
+
+valor = float(input("valor do produto: "))
+
+while True:
+
+    print(
+        "Qual a forma de pagamento? \n"
+        "Digite: (1)Pix (2)Cartão a Vista (3)Parcelado"
+        )
+
+    pagamneto = input()
+
+    def pix(valor):
+        desconto = valor - (valor / 100 * 15)
+        return desconto
+
+    def cartao(valor):
+        desconto = valor - (valor / 100 * 10)
+        return desconto
+
+    def parcelado(valor):
+        montante = valor * (1 + 0.10) ** parcelas
+        return montante
+
+
+
+    if pagamneto == "3":
+        print("Quantas parcelas? ")
+        try:
+            parcelas = float(input())
+        except ValueError:
+            print("coloque numeros")
+            continue
+        if parcelas <= 2:
+            print(f"Valor Final:{valor}")
+            break
+        if parcelas > 2:
+            print(f"Produto sai com 10% de juros.\n Valor Final:{parcelado(valor):.2f}")
+            break
+            
+        
+    if pagamneto == "2":
+        print(f"Produto sai com 10% de desconto.\n Valor Final:{cartao(valor)}")
+        break
+
+    if pagamneto == "1":
+        print(f"Produto sai com 15% de desconto. \n Valor Final:{pix(valor)}")
+        break
+        
+    if pagamneto != "1" and "2" and "3":
+        print("porfavor coloque um numero conrespondente com a forma de pagamento")
+        continue
+
+    
