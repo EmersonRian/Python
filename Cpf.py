d4490079adb39c882e824ade44937258fe55c702
EmersonRian <emersonrianlima06@gmail.com>
2026-03-20 17:30:48 -0300

    cpf

---
+++ Cpf.py
@@ -0,0 +1,86 @@
+"""
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+
+cpf = '746.824.890-70'
+# cpf = "061.565.203-41"
+cpf = '746824890'
+
+numeros_cpf_separados = []
+multiplicavel_1 = 10
+multiplicavel_2 = 11
+soma_numeros_primeiro_digito = []
+soma_numeros_sugundo_digito = []
+
+
+
+
+
+if len(cpf) > 11:
+    for i in cpf:
+        if i == "." or i == "-":
+            continue 
+
+        numeros_cpf_separados.append(int(i))
+        
+else:
+    for i in cpf:
+        numeros_cpf_separados.append(int(i))
+    
+
+for a in numeros_cpf_separados[:9]:
+    x = int(a) * multiplicavel_1
+    multiplicavel_1 -= 1
+    soma_numeros_primeiro_digito.append(x)
+
+valor_primeiro =  sum(soma_numeros_primeiro_digito) * 10 % 11
+primeiro_digito_cpf = valor_primeiro if valor_primeiro <= 9 else 0
+
+
+if primeiro_digito_cpf is not numeros_cpf_separados[-2]:
+    numeros_cpf_separados.append(primeiro_digito_cpf)
+
+
+
+for i in numeros_cpf_separados[:10]:
+    x = int(i) * multiplicavel_2
+    multiplicavel_2 -= 1
+    soma_numeros_sugundo_digito.append(x)
+
+valor_segundo = sum(soma_numeros_sugundo_digito) * 10 % 11
+segundo_digito_cpf = valor_segundo if valor_segundo <= 9 else 0
+
+numeros_cpf_separados.append(segundo_digito_cpf)
+
+cpf_regularizado = numeros_cpf_separados
+
+for i in cpf_regularizado:
+        print(i,end="")
+
+
+    
+
+
--- testes.py
+++ testes.py
@@ -1,40 +1,52 @@
-import random
-import os
+# import random
+# import os
 
 
-palavras_aleatorias = [
+# palavras_aleatorias = [
     
-    {"name": "aleatorio", "itens": ["Cogumelo","Hipopotamo","Passageiro","Pokemon","Jornada","açogueiro"]},
+#     {"name": "aleatorio", "itens": ["Cogumelo","Hipopotamo","Passageiro","Pokemon","Jornada","açogueiro"]},
 
-    {"nome": "animais", "itens": ["aguia","papagaio","leoa","tataruga"]},
+#     {"nome": "animais", "itens": ["aguia","papagaio","leoa","tataruga"]},
                        
-    {"nome": "comidas", "itens": ["cogumelo","pipoca","suchi","macarronada"]},
+#     {"nome": "comidas", "itens": ["cogumelo","pipoca","suchi","macarronada"]},
 
-    {"nome": "empregos", "itens": ["Farmaceutico", "Policial", "Dentista", "Açogueiro"]},
+#     {"nome": "empregos", "itens": ["Farmaceutico", "Policial", "Dentista", "Açogueiro"]},
 
-    {"nome": "Animes", "itens": ["Naruto", "Pokemon", "Dragon Ball", "One Piece"]},
+#     {"nome": "Animes", "itens": ["Naruto", "Pokemon", "Dragon Ball", "One Piece"]},
                        
                        
-]
+# ]
 
-erros = 0
-acertos = 0
-letras_tentadas = []
-while True:
-    print("Qual categoria de jogo vai ser? \n" "(1)Animais (2)Comidas (3)Empregos (4)Anime")
-    escolher = input()
+# erros = 0
+# acertos = 0
+# letras_tentadas = []
+# while True:
+#     print("Qual categoria de jogo vai ser? \n" "(1)Animais (2)Comidas (3)Empregos (4)Anime")
+#     escolher = input()
 
-    if escolher.isdigit():
-        break
-    else:
-        os.system("cls")
-        print("Por favor coloque um numero correspondente as alternativas")
-        continue
+#     if escolher.isdigit():
+#         break
+#     else:
+#         os.system("cls")
+#         print("Por favor coloque um numero correspondente as alternativas")
+#         continue
 
-alternativa = int(escolher) - 1
+# alternativa = int(escolher) - 1
 
-palavra_escolhida = random.choice(palavras_aleatorias[alternativa]["itens"]).lower()
+# palavra_escolhida = random.choice(palavras_aleatorias[alternativa]["itens"]).lower()
 
-print(palavra_escolhida)
+# print(palavra_escolhida)
+palavras = ["python", "asimov", "código", "web", "programação"]
+
+palavra_maior = palavras[0]
+palavra_menor = palavras[0]
 
 
+for iten in palavras:
+    if len(iten) > len(palavra_maior):
+        palavra_maior = iten
+    if len(iten) < len(palavra_menor):
+        palavra_menor = iten
+
+print(f"Menor palavra {palavra_menor}")
+print(f"Maior palavra {palavra_maior}")
